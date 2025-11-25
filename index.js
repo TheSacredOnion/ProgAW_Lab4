253b1817f1986c19899981fbea5e3a17a3336be7
Adam Kamiński <adam29kaminski@gmail.com>
2025-11-25 09:22:25 +0100

    Com4_1.3

--- index.js
+++ index.js
@@ -22,7 +22,25 @@ app.get('/math/rectangle/:a/:b', (req, res) => {
 });
 
 //TODO3
+app.get('/math/power/:base/:exponent', (req, res) => {
+  const base = parseFloat(req.params.base);
+  const exponent = parseFloat(req.params.exponent);
+  const rootVar = req.query.root || 'false';
 
+  if (isNaN(base) || isNaN(exponent) || (base < 0) && rootVar === 'true') {
+    return res.status(400).json({ error: 'Invalid input' });
+  }
+
+  const result = Math.pow(base, exponent);
+  if (rootVar === 'false') {
+    res.json({ result });
+  }
+  else {
+    const rootValue = Math.pow(base, 1 / 2);
+    res.json({ result, root : rootValue });
+  }
+  
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
